ca32bac685735c69f9c91f4c2e6cd88e86cd23c0
Arnaud Béguin <abeguin@kleis.ch>
2023-12-09 09:32:59 +0100

    init project

---
+++ day1/src/main/kotlin/abe/aoc/day1/Day1Application.kt
@@ -0,0 +1,11 @@
+package abe.aoc.day1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Day1Application
+
+fun main(args: Array<String>) {
+    runApplication<Day1Application>(*args)
+}
